3c7d718978f95e42881c15128a650f32ebe6723a
tmdgusya <dev0jsh@gmail.com>
2025-05-25 00:25:33 +0900

    array

--- array/main.go
+++ array/main.go
@@ -8,4 +8,14 @@ func main() {
 	for i := 0; i < len(t); i++ {
 		fmt.Println(t[i])
 	}
+
+	days := [3]string{"monday", "tuesday", "wednesday"}
+	for i := 0; i < len(days); i++ {
+		fmt.Println(days[i])
+	}
+
+	var s = [5]int{1: 10, 3: 30} // 인덱스 1에 10, 인덱스 3에 30을 할당하는 배열 선언
+	for i := 0; i < len(s); i++ {
+		fmt.Println(s[i])
+	}
 }
