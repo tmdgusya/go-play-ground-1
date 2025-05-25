267a665ff55aa0bca4d19027a9a358733071bc01
tmdgusya <dev0jsh@gmail.com>
2025-05-25 11:17:19 +0900

    call by value, call by reference

--- pointer/main.go
+++ pointer/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type Data struct {
+	value int
+	data  [200]int
+}
+
+func ChangeData(arg Data) {
+	arg.value = 100
+	arg.data[0] = 100
+}
+
+func ChangeDataPointer(arg *Data) {
+	arg.value = 100
+	arg.data[0] = 100
+}
+
 // pointer 는 메모리 주소를 값으로 가지는 타입이다.
 func main() {
 	var a int = 10
@@ -32,4 +47,15 @@ func main() {
 	// p0 := unsafe.Pointer(&arr[0])
 	// 2) uintptr로 변환하여 바이트 오프셋을 더함
 	// p1 := unsafe.Pointer(uintptr(p0) + unsafe.Sizeof(arr[0]))
+
+	var data Data
+
+	ChangeData(data)                                  // ChangeData 함수는 Data 구조체의 복사본을 받는다.
+	fmt.Println("Data value:", data.value)            // Data value: 0
+	fmt.Println("Data last element:", data.data[199]) // Data last element: 0
+	// ChangeData 함수는 data의 복사본을 받았기 때문에, 원본 data는 변경되지 않는다.
+	// Go 는 기본적으로 값에 의한 전달 방식을 사용한다. 즉, 함수에 인자를 전달할 때, 해당 인자의 복사본이 전달된다.
+	ChangeDataPointer(&data)                                                 // ChangeDataPointer 함수는 Data 구조체의 포인터를 받는다.
+	fmt.Println("Data value after ChangeDataPointer:", data.value)           // Data value after ChangeDataPointer: 100
+	fmt.Println("Data first element after ChangeDataPointer:", data.data[0]) // Data first element after ChangeDataPointer: 100
 }
